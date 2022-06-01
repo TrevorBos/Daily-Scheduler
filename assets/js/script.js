303b38cc9c75eed63d75165152a216e375ed925d
Trevor Bos <trevor.bos@outlook.com>
2022-06-01 19:05:21 -0400

    added the saveInputArray, and the ability to save to local storage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,52 @@
 // This ensures that the document is fully loaded first before anything else runs.
-$(document).ready(function(){
-    
-// Declaring variables and any arrays needed
+$(document).ready(function () {
+  // Declaring variables and any arrays needed
 
-// Variables
-var currentDay = $("#currentDay");
-var dayTimeDisplay = moment().format("MMMM Do YYYY, h:mm:ss a");
-var currentHour = moment().format("H");
+  // Variables
+  var currentDay = $("#currentDay");
+  var dayTimeDisplay = moment().format("MMMM Do YYYY, h:mm:ss a");
+  var currentHour = moment().format("H");
 
-// Arrays
+  // Arrays
 
-// Array to hold the days plans
-var userTextArray = [];
-// Array for the work day hours (might go a couple hours before and after for other scheduling fun)
-var hoursArray = ["7 AM","8 AM","9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM"];
+  // Array to hold the days plans
+  var userTextArray = [];
+  // Array for the work day hours (might go a couple hours before and after for other scheduling fun)
+  var hoursArray = [
+    "7 AM",
+    "8 AM",
+    "9 AM",
+    "10 AM",
+    "11 AM",
+    "12 PM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4 PM",
+    "5 PM",
+    "6 PM",
+    "7 PM",
+  ];
 
-//Sets the currentDay variable to the text version of dayTimeDisplay
-currentDay.text(dayTimeDisplay);
+  //Sets the currentDay variable to the text version of dayTimeDisplay
+  currentDay.text(dayTimeDisplay);
+  //Function call gets the saved data from local and puts it into the textarray
+  savePage();
 
-// Function for saving the data to the local array so that the data stays upon reload
+  // Function for saving the data to the local array so that the data stays upon reload
+  function savePage() {
+    //parsing the JSON into a string object
+    var userData = JSON.parse(localStorage.getItem("savedInput"));
+    //if the data was found, update the information in the array
+    if (userData !== null) {
+      userTextArray = userData;
+      console.log(userTextArray);
+    }
+  }
+  // Function for holding the text in an array
+  function saveInputArray() {
+    localStorage.setItem("savedInput", JSON.stringify(userTextArray));
+  }
 
-// Function for holding the text in an array
-
-// Loop, most likely a forEach type, to loop through and create the rows for every hour on the plan
+  // Loop, most likely a forEach type, to loop through and create the rows for every hour on the plan
 });
-
