9e1063cfbdda50bb3d3e4113a8bbee309a498b54
Trevor Bos <trevor.bos@outlook.com>
2022-06-01 18:22:09 -0400

    created initial variables / arrays.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,27 @@
+// This ensures that the document is fully loaded first before anything else runs.
+$(document).ready(function(){
+    
 // Declaring variables and any arrays needed
 
-// Most likely need an array to hold the days plans
+// Variables
+var currentDay = $("#currentDay");
+var dayTimeDisplay = moment().format("MMMM Do YYYY, h:mm:ss a");
+var currentHour = moment().format("H");
 
-// Another array for the work day hours (might go a couple hours before and after for other scheduling fun)
+// Arrays
+
+// Array to hold the days plans
+var userTextArray = [];
+// Array for the work day hours (might go a couple hours before and after for other scheduling fun)
+var hoursArray = ["7 AM","8 AM","9 AM","10 AM","11 AM","12 PM","1 PM","2 PM","3 PM","4 PM","5 PM","6 PM","7 PM"];
+
+//Sets the currentDay variable to the text version of dayTimeDisplay
+currentDay.text(dayTimeDisplay);
 
 // Function for saving the data to the local array so that the data stays upon reload
 
 // Function for holding the text in an array
 
 // Loop, most likely a forEach type, to loop through and create the rows for every hour on the plan
+});
+
