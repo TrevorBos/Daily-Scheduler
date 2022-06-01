eab46485049daf948823995fd98bdf8583dbea86
Trevor Bos <trevor.bos@outlook.com>
2022-06-01 19:37:27 -0400

    Added the 24hour system into the loop which converts the array. Also added the color changing based on the time of day, past, future or present.

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,6 @@ $(document).ready(function () {
     //if the data was found, update the information in the array
     if (userData !== null) {
       userTextArray = userData;
-      console.log(userTextArray);
     }
   }
   // Function for holding the text in an array
@@ -50,31 +49,37 @@ $(document).ready(function () {
 
   // Loop, most likely a forEach type, to loop through and create the rows for every hour on the plan
 
-  rowArray.forEach(function(){
-      //declare variables needed to set up the rows
-      var timeRow = $("<tr>");
-      var timeData =  $("<td>");
-      var textInfo = $("<input>");
-      var saveInfo = $("<button>");
+  rowArray.forEach(function (element, index) {
+    //declare variables needed to set up the rows
+    var timeRow = $("<tr>");
+    var timeData = $("<td>");
+    var textInfo = $("<input>");
+    var saveInfo = $("<button>");
 
-      //adding classes to each element of the row, also giving them some specific styling to go along with it
-      timeRow.addClass("row");
-      timeData.addClass("hour");
-      textInfo.addClass("textarea");
-      saveInfo.addClass("saveBtn fas fa-save");
+    //adding classes to each element of the row, also giving them some specific styling to go along with it
+    timeRow.addClass("row");
+    timeData.addClass("hour");
+    textInfo.addClass("textarea");
+    saveInfo.addClass("saveBtn fas fa-save");
 
-    //   We now want to convert the hours in our array into the 24hour format
-
-
+    // We now want to convert the hours in our array into the 24hour format
+    var hours = parseInt(currentHour);
+    if (element.includes("PM")) {
+        var elementHour = parseInt(element)+12;
+    }else{
+        elementHour = parseInt (element);
+    }
     // Assign colors to each box based off the time
-
+    if (hours > elementHour) {
+        textInfo.addClass("past");
+    }else if (hours < elementHour) {
+        textInfo.addClass("future");
+    }else if (hours === elementHour) {
+        textData.addClass("present")
+    }
 
     // Render saved information on refresh
 
-
-    //Save the information to the array and then to the local storage.
-
-    
-  })
-
+    // Save the information to the array and then to the local storage.
+  });
 });
