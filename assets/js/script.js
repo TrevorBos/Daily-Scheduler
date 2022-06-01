4da1062632a00f9cf9f719d6ad8be9c500072a7a
Trevor Bos <trevor.bos@outlook.com>
2022-06-01 17:32:23 -0400

    added some pseudo coding to the JS file to figure out what might be needed. Will add GH Issues based off this.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Declaring variables and any arrays needed
+
+// Most likely need an array to hold the days plans
+
+// Another array for the work day hours (might go a couple hours before and after for other scheduling fun)
+
+// Function for saving the data to the local array so that the data stays upon reload
+
+// Function for holding the text in an array
+
+// Loop, most likely a forEach type, to loop through and create the rows for every hour on the plan
